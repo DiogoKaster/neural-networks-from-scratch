d065ce81a440a9824dd874cbae20de59ea8a363b
DiogoKaster <diogokaster@gmail.com>
2025-02-10 22:08:36 -0300

    first training data and dense layer class created

--- main.py
+++ main.py
@@ -1,20 +1,16 @@
 import matplotlib.pyplot as plt
+import nnfs
+import nnfs.datasets
 import numpy as np
 
-inputs = [1, 2, 3, 2.5]
-weights = [[0.2, 0.8, -0.5, 1], [0.5, -0.91, 0.26, -0.5], [-0.26, -0.27, 0.17, 0.87]]
-biases = [2, 3, 0.5]
+from core.dense_layer import Layer_Dense
 
-layer_output = []
+nnfs.init()
 
-for neuron_weights, neuron_bias in zip(weights, biases):
-    neuron_output = 0
+X, y = nnfs.datasets.spiral_data(samples=100, classes=3)
 
-    for n_input, weight in zip(inputs, neuron_weights):
-        neuron_output += n_input * weight
+dense1 = Layer_Dense(2, 1)
 
-    neuron_output += neuron_bias
+dense1.forward(X)
 
-    layer_output.append(neuron_output)
-
-print(layer_output)
+print(dense1.output[:5])
